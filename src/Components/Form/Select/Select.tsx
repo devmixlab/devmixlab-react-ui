530de1b3c9d565c84fe385859706a1b7d46939de
Roman <devmixlab@gmail.com>
2026-06-05 20:35:36 +0300

    fix

--- src/Components/Form/Select/Select.tsx
+++ src/Components/Form/Select/Select.tsx
@@ -1,6 +1,6 @@
 import React, { forwardRef, useRef } from 'react';
 import clsx from 'clsx';
-import { Box, type BoxProps } from '../../Box/Box';
+import { Box, type BoxProps } from '../../Box';
 import { Size } from '../form.tokens';
 import { useFormFieldContext } from '../FormField/FormField.context';
 import { TriangleDown as TriangleDownIcon } from '../../../Icon';
--- src/Components/Form/index.ts
+++ src/Components/Form/index.ts
@@ -2,3 +2,4 @@ export * from './TextInput';
 export * from './FormField';
 export * from './Textarea';
 export * from './Switch';
+export * from './Select';
